abef8bab309a142aa6acb867ef592fd0c0b82e1a
Bryton Lee <brl013@ucsd.edu>
2025-09-25 00:30:13 -0700

    updated dashing, need cleanup and fix jump of walls

--- HG-Game/Assets/Scripts/playerMovement.cs
+++ HG-Game/Assets/Scripts/playerMovement.cs
@@ -3,32 +3,57 @@
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class playerMovement : MonoBehaviour
 {
-  [SerializeField] private InputActionAsset InputActions;
-
+  // Input actions
+  private InputActionAsset InputActions;
   private InputAction m_moveAction;
   private InputAction m_jumpAction;
   private InputAction m_dashAction;
 
-  [SerializeField] private Rigidbody2D body;
+  // Parts of the player
+  private Rigidbody2D body;
   [SerializeField] private Transform groundCheck;
-  private float groundCheckRadius = 0.04f;
+  [SerializeField] private Transform leftCheck;
+  [SerializeField] private Transform rightCheck;
+  // Layers
   [SerializeField] private LayerMask groundLayer;
+  [SerializeField] private LayerMask wallLayer;
 
+  // Ground check parameters
+  private float groundCheckRadius = 0.04f;
+
+  // Basic movement storage and parameters
   private Vector2 moveInput;
   private float speed = 4f;
-  private float jumpingPower = 9f;
+
+  //Gravity
   private float defaultGravity = -9.81f;
-  private float scaleGravity = 4;
+  private float scaleGravity = 4f;
+
+  // Jump
+  [SerializeField] private int jumpsAvailiable = 2;
+
+  private int maxJumps = 2;
+  private float jumpingPower = 16f;
+
+  // Dash
   private bool dashAvailable = true;
-  private float dashTime = 0.3f;
+  private float dashTime = 0.15f;
   private bool isDashing = false;
-  private float dashPower = 6f;
+  private float dashPower = 12f;
   private float dashEnd = 0f;
   private float dashDirection = 0;
-  private bool doubleJumpReset = true;
+
+  // Wall mechanics
+  [SerializeField] private float wallStickEnd = 0.0f;
+  private float wallStickDuration = 2f;
+  [SerializeField] private bool isOnWall = false;
+  [SerializeField] private bool wallSticky = false;
+  private float scaleStickyGravity = 2f;
+  private float wallDirection = 0;
 
   private void Awake()
   {
@@ -43,7 +68,7 @@ public class playerMovement : MonoBehaviour
     moveInput = m_moveAction.ReadValue<Vector2>();
     // Debug.Log(moveInput);
 
-    if (m_jumpAction.WasPressedThisFrame() && (isGrounded() || doubleJumpReset))
+    if (m_jumpAction.WasPressedThisFrame() && (isGrounded() || jumpsAvailiable > 0))
     {
       Jump();
     }
@@ -56,11 +81,16 @@ public class playerMovement : MonoBehaviour
 
   private void Jump()
   {
-    if (!isGrounded())
+    jumpsAvailiable -= 1;
+    if (isOnWall && (wallDirection != moveInput.x))
     {
-      doubleJumpReset = false;
+      body.linearVelocity = new Vector2(body.linearVelocity.x, 0);
     }
-    body.linearVelocity = new Vector2(body.linearVelocity.x, 0);
+    else
+    {
+      body.linearVelocity = new Vector2(body.linearVelocity.x, 0);
+    }
+    
     body.AddForceAtPosition(new Vector2(0, jumpingPower), Vector2.up, ForceMode2D.Impulse);
   }
 
@@ -73,14 +103,25 @@ public class playerMovement : MonoBehaviour
     dashEnd = Time.time + dashTime;
   }
 
-  // private void OnCollisionEnter2D(Collision2D collision)
-  // {
-  //   switch (collision.gameObject.layer)
-  //   {
-  //     default:
-  //       break;
-  //   }
-  // }
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    switch (collision.gameObject.layer)
+    {
+      case 8:
+        jumpsAvailiable = 2;
+        break;
+      case 9:
+        wallDirection = wallCollisionDirection();
+        isOnWall = true;
+        wallStickEnd = Time.time + wallStickDuration;
+        dashAvailable = true;
+        jumpsAvailiable = maxJumps;
+        wallSticky = true;
+        break;
+      default:
+        break;
+    }
+  }
 
   private void OnCollisionStay2D(Collision2D collision)
   {
@@ -88,8 +129,15 @@ public class playerMovement : MonoBehaviour
     {
       case 8:
         dashAvailable = true;
-        doubleJumpReset = true;
         // Debug.Log("ground layer");
+        break;
+      case 9:
+        if (wallStickEnd < Time.time)
+        {
+          wallSticky = false;
+        }
+        // Debug.Log("walled");
+
         break;
       default:
         break;
@@ -97,10 +145,26 @@ public class playerMovement : MonoBehaviour
     // Debug.Log("Collision stay with" + collision.gameObject.name);
   }
 
-  // private void OnCollisionExit2D(Collision2D collision)
-  // {
-  //   Debug.Log("Collision exit with" + collision.gameObject.name);
-  // }
+  private void OnCollisionExit2D(Collision2D collision)
+  {
+    switch (collision.gameObject.layer)
+    {
+      case 8:
+        if (jumpsAvailiable == maxJumps && isGrounded())
+        {
+          jumpsAvailiable -= 1;
+        }
+
+        break;
+      case 9:
+        isOnWall = false;
+        goto case 8;
+        // break;
+      default:
+        break;
+    }
+    // Debug.Log("Collision exit with" + collision.gameObject.name);
+  }
 
   private void FixedUpdate()
   {
@@ -112,26 +176,64 @@ public class playerMovement : MonoBehaviour
       }
       // else
       // {
-        
+
       // }
     }
 
     if (isDashing)
     {
-      Debug.Log("test");
       body.linearVelocity = new Vector2(dashDirection * dashPower, 0);
     }
     else
     {
-      body.linearVelocity = new Vector2(moveInput.x * speed, body.linearVelocity.y);
-      body.linearVelocity += new Vector2(0, defaultGravity * scaleGravity * Time.fixedDeltaTime);
+      if (wallSticky && isOnWall)
+      {
+        body.linearVelocity = new Vector2(moveInput.x * speed, 0);
+        body.linearVelocity += new Vector2(0, 0);
+      }
+      else if (isOnWall && !wallSticky)
+      {
+        body.linearVelocity = new Vector2(0, body.linearVelocity.y);
+        if (moveInput.x != wallDirection)
+        {
+          body.linearVelocity += new Vector2(moveInput.x * speed,  defaultGravity * scaleGravity * Time.fixedDeltaTime);
+        }
+        else
+        {
+          body.linearVelocity += new Vector2(0,  defaultGravity * scaleStickyGravity * Time.fixedDeltaTime);
+        }
+        
+      }
+      else
+      {
+        body.linearVelocity = new Vector2(moveInput.x * speed, body.linearVelocity.y);
+        body.linearVelocity += new Vector2(0, defaultGravity * scaleGravity * Time.fixedDeltaTime);
+      }
+
     }
-    
+    // Debug.Log(body.linearVelocity);
+
   }
 
   private bool isGrounded()
   {
     return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     // return false;
+  }
+
+  private float wallCollisionDirection()
+  {
+    if (Physics2D.OverlapCircle(leftCheck.position, groundCheckRadius, wallLayer))
+    {
+      Debug.Log("left");
+      return -1f;
+    }
+    if (Physics2D.OverlapCircle(rightCheck.position, groundCheckRadius, wallLayer))
+    {
+      Debug.Log("right");
+      return 1f;
+    }
+
+    return 0f;
   }
 }
